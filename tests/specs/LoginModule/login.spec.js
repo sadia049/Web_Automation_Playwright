1499fc6a566380d81ad083221e975c460fc3a81f
sadia049 <sadianawsin018@gmail.com>
2026-06-28 02:51:03 +0600

    Edited gitignore and Login Module

--- tests/specs/LoginModule/login.spec.js
+++ tests/specs/LoginModule/login.spec.js
@@ -3,22 +3,22 @@ import { Login } from '../../pageObject/LoginPage/login.js'
 import LoginData from '../../TestData/LoginData/login.json' with { type: 'json' };
 
 
-// test('Verify Login with valid username and password', async({page})=>{
+test('Verify Login with valid username and password', async({page})=>{
 
-//     const login_object =  new Login(page);
-//     await login_object.verifyLogin(LoginData.valid_username, LoginData.valid_password);
+    const login_object =  new Login(page);
+    await login_object.verifyLogin(LoginData.valid_username, LoginData.valid_password);
     
-//     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 
-// })
+})
 
-// test('Verify Login with invalid username and password', async({page})=>{
+test('Verify Login with invalid username and password', async({page})=>{
 
-//     const login_object = new Login(page);
-//     await login_object.verifyLogin(LoginData.invalid_username, LoginData.invalid_password);
-//     const error_text = login_object.error_message.innerText();
-//     await expect(error_text).toBeTruthy()
-// })
+    const login_object = new Login(page);
+    await login_object.verifyLogin(LoginData.invalid_username, LoginData.invalid_password);
+    const error_text = await login_object.error_message.innerText();
+    await expect(error_text).toBeTruthy()
+})
 
 for(const user of LoginData.users){
 
